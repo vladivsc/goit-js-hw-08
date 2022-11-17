65056c3bc0bef9b9ee383c6a409b2a7a2a2d0f61
Vladyslav <101827554+vladivsc@users.noreply.github.com>
2022-11-17 15:07:47 +0100

    upd task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
 const throttle = require('lodash.throttle');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('[name="message"]'),
+};
+
+const formData = {};
+
+savedMessageFromLocalStorage();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaChange, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const userEmail = refs.form.elements.email.value;
+  const userMessage = refs.form.elements.message.value;
+
+  formData[userEmail] = userMessage;
+  console.log(formData);
+
+  evt.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function savedMessageFromLocalStorage(evt) {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
+
+function onTextareaChange(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
